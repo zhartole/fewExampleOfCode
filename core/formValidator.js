27437e9bce15a526de70a4eef2db4e95d1e696a8
Артем Жмакін <a.zhmakin@softimus.local>
2016-04-20 10:38:35 +0100

    example

---
+++ core/formValidator.js
@@ -0,0 +1,95 @@
+var formValidator = (function(){
+	
+	var EMAIL_REGEXP = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+	var usersArray = [];
+	var userId = 0;
+	var DOMElements = null;
+	
+	function elementDisplay(elem, display) { elem.style.display = display ? '' : 'none' }
+
+	function hideMessages() {
+		elementDisplay(DOMElements.errorMsg, false);
+		elementDisplay(DOMElements.successMsg, false);
+	}
+
+	function resetForm() {
+		hideMessages();
+		elementDisplay(DOMElements.result, false);
+		elementDisplay(DOMElements.form, true);
+		DOMElements.name.value = '';
+		DOMElements.email.value = '';
+		DOMElements.password.value = '';
+	};
+
+	function getPasswdByUserId(id) {
+		var pass ='';
+		usersArray.forEach(function(item) {
+			if (item.id == id) { pass = item.password };
+		});
+		return pass;
+	}
+
+	function changePassword(event) {
+		var password = getPasswdByUserId(event.target.id);
+		if (event.target.type == 'button' && event.target.getAttribute("class") == 'btn btn-info') {
+			event.target.parentElement.previousSibling.innerHTML = password;
+			event.target.setAttribute("class", "btn btn-warning");
+			event.target.innerHTML = "Скрыть пароль";
+		} else if (event.target.type == 'button' && event.target.getAttribute("class") == 'btn btn-warning') {
+			event.target.parentElement.previousSibling.innerHTML = password.replace(/[\s\S]/g, '*');
+			event.target.setAttribute("class", "btn btn-info");
+			event.target.innerHTML = "Показать пароль";
+		}
+	}
+
+	function showTable(){
+		DOMElements.tableContent.innerHTML = "";
+		usersArray.forEach(function(item, i) {
+		DOMElements.tableContent.innerHTML += "<tr><td>" + item.id + "</td><td>" + item.name + "</td><td>" + item.email + "</td>" 
+  												+ "<td>" + item.password.replace(/[\s\S]/g, '*') + "</td>" 
+  												+ "<td><button id='" + item.id + "' type='button' class='btn btn-info'>Показать пароль</button></td></tr>";
+		});
+		elementDisplay(DOMElements.form, false);
+		elementDisplay(DOMElements.result, true);
+	}
+
+	function checkForm (event) {
+		hideMessages();
+		event.preventDefault();
+		if ( DOMElements.name.value 
+			 && DOMElements.password.value 
+			 && EMAIL_REGEXP.test(DOMElements.email.value)) {
+			 	usersArray.push({id : ++userId, name : DOMElements.name.value, email : DOMElements.email.value, password : DOMElements.password.value});			
+			 	elementDisplay(DOMElements.successMsg, true);
+				showTable();
+		} else {
+			elementDisplay(DOMElements.errorMsg, true);
+		}
+	}
+	
+	return {
+		setForm : function(form){	
+			DOMElements = form;
+		},
+		initValidator: function(){
+			DOMElements.submitBtn.addEventListener( "click", checkForm );
+			DOMElements.resetBtn.addEventListener( "click", resetForm );
+			DOMElements.tableContent.addEventListener( "click", changePassword );
+		}
+	}
+}())
+
+formValidator.setForm({
+	name 	: document.querySelector("#inputName"),
+	email 	: document.querySelector("#inputEmail"),
+	password : document.querySelector("#inputPassword"),
+	result : document.querySelector("#result"),
+	form : document.querySelector("#form"),
+	successMsg : document.querySelector("#successMsg"),
+	errorMsg : document.querySelector("#errorMsg"),
+	tableContent : document.querySelector("#table-content"),
+	submitBtn : document.querySelector("#submitBtn"),
+	resetBtn : document.querySelector("#resetBtn"),
+})
+
+formValidator.initValidator();
---
+++ react/contact-list/data.js
@@ -0,0 +1,23 @@
+var CONTACTS = [
+	{
+		id: 1,
+		name: 'Sheldon Cooper',
+		phoneNumber: '+120966666666',
+		image: 'http://dota2.ru/forum/data/avatars/l/573/573850.jpg?1440672151'
+	}, {
+		id: 2,
+		name: 'Leonard Hofstadter',
+		phoneNumber: '+120966344466',
+		image: 'http://coolspotters.com/files/photos/718528/leonard-hofstadter-profile.jpg?1357434741'
+	}, {
+		id: 3,
+		name: 'Howard Wolowitz',
+		phoneNumber: '+120976654433',
+		image: 'http://www.jewornotjew.com/img/people/h/howard_wolowitz.jpg'
+	}, {
+		id: 4,
+		name: 'Rajesh Koothrappali',
+		phoneNumber: '+120456784935',
+		image: 'http://thebigbangtheoryfansite.com/wp-content/uploads/2013/02/raj.jpg'
+	}
+];
